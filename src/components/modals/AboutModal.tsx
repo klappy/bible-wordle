c797f0516bdb4cab630f8df3307ba0dfff262d92
klappy <christopher@klapp.name>
2022-01-31 19:33:21 -0500

    fixed whitespace and open in a new tab.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -10,16 +10,24 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
         This is an open source clone of the game Wordle -{' '}
-        <a href="https://github.com/klappy/wordle" className="underline font-bold">
+        <a href="https://github.com/klappy/wordle" className="underline font-bold" target='_blank' rel='noreferrer'>
           check out the code here
         </a>{' '}
         and{' '}
-        <a href="https://www.powerlanguage.co.uk/wordle/" className="underline font-bold">
+        <a href="https://www.powerlanguage.co.uk/wordle/" className="underline font-bold" target='_blank' rel='noreferrer'>
           play the original here.
         </a>
       </p>
       <p className="text-sm text-gray-500">
-        The word list is created from the <a href='https://www.unfoldingword.org/ult' className='underline font-bold'>ULT</a> translation by <a href='https://www.unfoldingword.org' className='underline font-bold'>unfoldingWord</a> used as a translation resource for Bible translators.
+        The word list is created from the{' '}
+        <a href='https://www.unfoldingword.org/ult' className='underline font-bold' target='_blank' rel='noreferrer'>
+          ULT
+        </a>{' '}
+        translation<br /> by {' '}
+        <a href='https://www.unfoldingword.org' className='underline font-bold' target='_blank' rel='noreferrer'>
+          unfoldingWord
+        </a>{' '}
+        used as a translation resource for Bible translators.
       </p>
     </BaseModal>
   )
