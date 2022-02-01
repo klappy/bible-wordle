49de260417c0e6f02be8dc2ee81ab0826bc07a0f
klappy <christopher@klapp.name>
2022-01-31 19:19:39 -0500

    About message to include description and links.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -10,19 +10,16 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
         This is an open source clone of the game Wordle -{' '}
-        <a
-          href="https://github.com/hannahcode/wordle"
-          className="underline font-bold"
-        >
+        <a href="https://github.com/klappy/wordle" className="underline font-bold">
           check out the code here
         </a>{' '}
         and{' '}
-        <a
-          href="https://www.powerlanguage.co.uk/wordle/"
-          className="underline font-bold"
-        >
-          play the original here
+        <a href="https://www.powerlanguage.co.uk/wordle/" className="underline font-bold">
+          play the original here.
         </a>
+      </p>
+      <p className="text-sm text-gray-500">
+        The word list is created from the <a href='https://www.unfoldingword.org/ult' className='underline font-bold'>ULT</a> translation by <a href='https://www.unfoldingword.org' className='underline font-bold'>unfoldingWord</a> used as a translation resource for Bible translators.
       </p>
     </BaseModal>
   )
