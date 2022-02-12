113cacb7022e938d69f6a903c1e29ab4ccf2a7e8
klappy <christopher@klapp.name>
2022-02-12 15:39:59 -0500

    Donation link added.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Wordle Clone
+# Bible Wordle - A Wordle Clone
 
 - Go play the real Wordle [here](https://www.powerlanguage.co.uk/wordle/)
 - Read the story behind it [here](https://www.nytimes.com/2022/01/03/technology/wordle-word-game-creator.html)
--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -29,6 +29,23 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
         </a>{' '}
         used as a translation resource for Bible translators.
       </p>
+      <p className="text-sm text-gray-500">
+        <button
+          type="button"
+          className="mx-auto mt-8 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
+          onClick={()=> window.open("https://unfoldingword.secure.force.com/donate/DonationPage?id=7014T000000gfudQAA", "_blank")}
+        >
+          Give to support open-licensed tools!
+        </button>
+        {' '}<br />
+      </p>
+      <p className="text-sm text-gray-500">
+          When you{' '}
+          <a href='https://www.unfoldingword.org' className='underline font-bold' target='_blank' rel='noreferrer'>
+            give to unfoldingWord
+          </a>
+          , you are equipping and empowering church networks worldwide with free and open-licensed tools to translate the Word of God with excellence.
+      </p>
     </BaseModal>
   )
 }
